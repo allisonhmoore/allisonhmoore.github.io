ef89d2b8800c5250d8d177f0fcc0563537ebdd77
allisonhmoore <allison.h.moore@gmail.com>
2025-01-25 02:37:37 +0000

    Deploying to gh-pages from @ allisonhmoore/allisonhmoore.github.io@e7e0623d6b0fc3e585d7ab548f53c23fd0dd9afd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,10 +18,24 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Info about classes I&#39;ve taught",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Info about classes I&#39;ve taught",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
           },
         },{id: "nav-teaching",
           title: "teaching",
